91bb56321a9b4b0639a68e83777cb52f04014f52
tomizdebski <tomizdebski@gmail.com>
2024-01-10 21:10:09 +0100

    add_new_site_wyrzuta

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -27,7 +27,7 @@ const projects = [
       "The app enables the donation of unnecessary items. You can create an account, post an advertisement, check the location, and finalize the transaction.",
     image: "/wyrzuta_app.png",
     github: "https://github.com/tomizdebski/wyrzuta_pl_frontend",
-    link: "http://wyrzuta.pl/",
+    link: "https://wyrzuta-pl-frontend.vercel.app/",
   },
   {
     name: "BulettinBoard App",
